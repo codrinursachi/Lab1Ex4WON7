827a724d5742149a4b0c4d01332ca21dcc6d963b
codri <codri@VIRTUAL>
2024-02-15 22:43:44 +0200

    Add project files.

---
+++ Lab1Ex4WON7/Program.cs
@@ -0,0 +1,7 @@
+﻿/*Ex 4
+Scrieti un program care va afisa semnul unui numar citit de la tastatura
+• Daca numarul este pozitiv, va afisa “+”
+• Daca numarul este negativ, va afisa “-”
+• Daca numarul este 0, va afisa “0”*/
+
+ShowSign(ReadNumber());
---
+++ Lab1Ex4WON7/ProgramHelpers.cs
@@ -0,0 +1,28 @@
+﻿partial class Program
+{
+    static int ReadNumber()
+    {
+        Console.WriteLine("Introduceti un numar pentru a i se afisa semnul");
+        if(!int.TryParse(Console.ReadLine(), out var number))
+        {
+            Console.WriteLine("Nu ati introdus datele in format valid, mai incercati");
+            return ReadNumber();
+        }
+        return number;
+    }
+    static void ShowSign(int number)
+    {
+        switch (number)
+        {
+            case >0: 
+                Console.WriteLine("+");
+                break;
+            case 0:
+                Console.WriteLine("0");
+                break;
+            default:
+                Console.WriteLine("-");
+                break;
+        }
+    }
+}
