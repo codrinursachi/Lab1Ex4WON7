6bf02ecb9cf1eee789ee3a4f4b7e0bd1b3f7a8fb
codrinursachi <codrinursachi@gmail.com>
2024-02-18 08:27:51 +0200

    Formatting

--- Lab1Ex4WON7/Program.cs
+++ Lab1Ex4WON7/Program.cs
@@ -1,7 +1,11 @@
-﻿/*Ex 4
-Scrieti un program care va afisa semnul unui numar citit de la tastatura
-• Daca numarul este pozitiv, va afisa “+”
-• Daca numarul este negativ, va afisa “-”
+﻿/*Ex 4
+
+Scrieti un program care va afisa semnul unui numar citit de la tastatura
+
+• Daca numarul este pozitiv, va afisa “+”
+
+• Daca numarul este negativ, va afisa “-”
+
 • Daca numarul este 0, va afisa “0”*/
 
 ShowSign(ReadNumber());
